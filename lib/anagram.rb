980f176db0632cf228b81ca56166a22b3fe6fb42
kellyeryan <kellyeryan@gmail.com>
2019-07-31 15:44:08 -0400

    Finish anagram lab

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+require "pry"
+
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+
+  def match(words)
+
+  words.find_all do |i|
+    (i.split("").sort) == word.split("").sort
+
+    end
+  end
+end
